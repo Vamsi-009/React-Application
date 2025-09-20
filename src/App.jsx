56d528addeaa7304fcc9019903929546cf7714dd
Vamsi-009 <vamsimamidi197@gmail.com>
2025-09-21 01:29:56 +0530

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Footer from "./Footer"
 import Food from "./Food"
 import Button from "./Button"
 import Card from "./Card"
+import Student from "./Student"
 function App() {
   return(
     <>
     <Header />
     <Food /> 
     <Card />
+    <Student name = "Toni" age ="25" isStudent = {true} />
+    <Student name = "rupendra" age ="24" isStudent = {true} />
     <Button />
     <Footer />
 
