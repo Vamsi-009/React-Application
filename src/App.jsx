84b51228d8edd4f6b0869260e20a071c4d751493
Vamsi-009 <vamsimamidi197@gmail.com>
2025-09-21 07:50:25 +0530

    conditional-redering

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Food from "./Food"
 import Button from "./Button"
 import Card from "./Card"
 import Student from "./Student"
+import UserGreeting from "./UserGreeting"
+
 function App() {
   return(
     <>
@@ -13,7 +15,9 @@ function App() {
     <Student name = "Toni" age ="25" isStudent = {true} />
     <Student name = "rupendra" age ="24" isStudent = {true} />
     <Button />
+    <UserGreeting isLoggedIn={true} username="Toni" />
     <Footer />
+
 
     </>
   )
---
+++ src/UserGreeting.jsx
@@ -0,0 +1,34 @@
+import { jsx } from 'react/jsx-runtime'; 
+import { jsxDEV } from 'react/jsx-dev-runtime';
+import propTypes from 'prop-types';        
+
+function UserGreeting(props){
+    // if(props.isLoggedIn){    
+    //     return <h2>Welcome to {props.username}</h2>  
+    // }
+    // else{
+    //     return <h2>please login to continue</h2>
+    // }
+
+
+    // return(props.isLoggedIn ? <h2 className="wel-msg">Welcome {props.username} </h2> : 
+    //                           <h2 className="log-msg">please login to countinue</h2> );
+
+
+
+
+    const welcomeMsg = <h2 className="wel-msg">Welcome {props.username} </h2>
+    const logMsg = <h2 className="log-msg">please login to countinue</h2>
+
+    return(props.isLoggedIn ? welcomeMsg : logMsg);
+}
+
+UserGreeting.propTypes = {
+    isLoggedIn : propTypes.bool,
+    username :propTypes.string,
+}
+
+UserGreeting.defaultprops ={
+
+}
+export default UserGreeting
